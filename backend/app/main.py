3bb1c400c32c34177a2e99ac2a929f69f8e264fc
Nithilaa011 <nithilaa011@example.com>
2026-01-05 11:49:34 +0530

    Add CORS middleware for frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routes import scoring
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="AI Credit Scoring Platform",
@@ -7,6 +8,14 @@ app = FastAPI(
     version="1.0.0"
 )
 app.include_router(scoring.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
